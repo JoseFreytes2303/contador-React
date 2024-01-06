690e9da707e92dcb2bac40cf3be3734239e38f6d
Jose Maria Freytes <josefreytes0904@gmail.com>
2024-01-06 08:44:52 -0300

    Contador hecho con React

---
+++ src/HookApp.jsx
@@ -0,0 +1,12 @@
+import { CounterComponent } from "./components/CounterComponent"
+
+
+export const HookApp = () => {
+    return (
+        <>
+            <h1>Aplicación de Hooks</h1>
+            <hr />
+            <CounterComponent></CounterComponent>
+        </>
+    )
+}
